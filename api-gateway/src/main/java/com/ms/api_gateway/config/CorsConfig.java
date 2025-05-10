f669728b110d35b2cfe7636a1727905e5a9ebceb
leonardo <leonardopsantos95@gmail.com>
2025-05-10 14:48:08 -0300

    add services and docker compose

---
+++ api-gateway/src/main/java/com/ms/api_gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.ms.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        System.out.println(">>>>>cors carregado!!!!");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:5500"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        System.out.println(">>>>> Configuração CORS: Origins=" + configuration.getAllowedOrigins() + ", Methods=" + configuration.getAllowedMethods() + ", Headers=" + configuration.getAllowedHeaders() + ", Credentials=" + configuration.getAllowCredentials());
+        return new CorsWebFilter(source);
+    }
+}
